f7a4649662a2a51f15379299c9cd3793f5751517
Koennemeloen2003 <146561189+Koennemeloen2003@users.noreply.github.com>
2024-03-14 12:46:55 +0100

    Add files via upload

--- Afstandsbediening.ino
+++ Afstandsbediening.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 
+#include "WiFi.h"
 
 //Screen set-up
 #define SCREEN_WIDTH 128
@@ -13,7 +14,7 @@ Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 //button Pins
 #define buttonOpen 5
 #define buttonClose 19
-#define buttonStop 13
+#define buttonStop 13 
 
 //led pins
 #define redLed 4
@@ -36,6 +37,11 @@ bool batteryLow;
 float currentLevel;
 int lierGear;
 int katGear;
+int maxVDeadzone;
+int minVDeadzone;
+int maxHDeadzone;
+int minHDeadzone;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -51,8 +57,16 @@ void setup() {
   pinMode(horLeft, INPUT);
   pinMode(battery, INPUT);
 
+  int midValueV= analogRead(vertLeft);
+  maxVDeadzone= midValueV+15;
+  minVDeadzone= midValueV-15;
+  int midValueH = analogRead(horRight);
+  maxHDeadzone = midValueH+15;
+  minVDeadzone = midValueH-15;
+
   Serial.begin(9600); //start seriele monitor
 
+  
     // initialize OLED display with address 0x3C for 128x64
   if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed")); // print een error uit als het upzette van het schermpje niet lukt
@@ -66,89 +80,49 @@ void setup() {
   oled.display();               // show on OLED
 
 }
+
+
 // Leest de analoge waarde van de joystick uit om zo te bepalen in welke versnelling de lier zit
 void updateLierGear(){
-    int temp = analogRead(vertLeft)*100/4095; // verandert de analoge input van de joystick naar een persentage
-    switch (temp){                            // kijkt in welke versnelling de lier zit op bassis van het persentage
-      case 0 ... 15:{
-        lierGear = -3;
-      }
-      break;
-      case 16 ... 30:{
-        lierGear =-2;
-      }
-      break;
-      case 31 ... 44:{
-        lierGear= -1;
-      }
-      break;
-      case 45 ... 55:{
-        lierGear = 0;
-      }
-      break;
-      case 56 ... 70:{
-        lierGear = 1;
-      }
-      break;
-      case 71 ... 85:{
-        lierGear = 2;
-      }
-      break;
-      case 86 ... 100:{
-        lierGear = 3;
-      }
-      break;
-
+    int temp = analogRead(vertLeft); 
+        if(temp< minVDeadzone){
+      lierGear=map(temp, 0, minVDeadzone, -650, -1);
     }
+    else if (temp > maxVDeadzone){
+      lierGear= map(temp, maxVDeadzone, 4095, 1, 650);
+    }
+    else{
+      lierGear =0;
+    }
+  
 }
 
 
 // Leest de analoge waarde van de joystick uit om zo te bepalen in welke versnelling de kat zit
 void updateKatGear(){
-    int temp = (analogRead(horRight)*100)/4095;     // verandert de analoge input van de joystick naar een persentage
-    switch (temp){                                  // kijkt in welke versnelling de kat zit op bassis van het persentage
-      case 0 ... 15:{
-        katGear = -3;
-      }
-      break;
-      case 16 ... 30:{
-        katGear =-2;
-      }
-      break;
-      case 31 ... 44:{
-        katGear= -1;
-      }
-      break;
-      case 45 ... 55:{
-        katGear = 0;
-      }
-      break;
-      case 56 ... 70:{
-        katGear = 1;
-      }
-      break;
-      case 71 ... 85:{
-        katGear = 2;
-      }
-      break;
-      case 86 ... 100:{
-        katGear = 3;
-      }
-      break;
-
+    int temp = analogRead(horRight); 
+    if(temp< minHDeadzone){
+      katGear=map(temp, 0, minHDeadzone, -650, -1);
     }
+    else if (temp > maxHDeadzone){
+      katGear= map(temp, maxHDeadzone, 4095, 1, 650);
+    }
+    else{
+      katGear =0;
+    }
+
 }
 
 //Update het persentage van de battery
 void batteryUpdate(){
   currentLevel=analogRead(battery); //leest de analoge waarde vanaf de spanningsdeler in
-  if (currentLevel -3164 > 0) {     // kijken of het persentage hoger wordt dan 0%
-    batteryPercent= ((currentLevel-3164)*100/(4095-3164));  //rekent het persentage van de battery uit
+  if (currentLevel -3164 > 0) {     // kijken of het persentage hoger wordt dan 0%  
+    batteryPercent= map(currentLevel, 3164, 4095, 0, 100); //rekent het persentage van de battery uit
   }
   else{
     batteryPercent = 0;
   }
-  if (batteryPercent < 25){       // als het persentage lager is dan 25 procent is dan gaat hij is lege battery modes
+  if (batteryPercent < 10){       // als het persentage lager is dan 10 procent is dan gaat hij is lege battery modes
     batteryLow = true;
   }
   else{
@@ -187,6 +161,8 @@ void screenUpdate(){
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
   if(screenTimer + 500 <= millis()) {     //vergetlijkt de huidige tijd met de laatste keer dat het scherm geupdate is
     batteryUpdate();                      // roept de functie om het batterypercentage te hernieuwen
     screenUpdate();                       // roept de functie aan om het scherm te hernieuwen
