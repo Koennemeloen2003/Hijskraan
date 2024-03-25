5bae69989d98dde2d76914a8aad9d953d927c6b3
Koennemeloen2003 <146561189+Koennemeloen2003@users.noreply.github.com>
2024-03-25 14:32:54 +0100

    Add files via upload

--- Afstandsbediening.ino
+++ Afstandsbediening.ino
@@ -1,9 +1,27 @@
 #include <Adafruit_SSD1306.h>
 #include <splash.h>
 #include <Wire.h>
+#include <esp_now.h>
 #include <Adafruit_GFX.h>
-
 #include "WiFi.h"
+
+// REPLACE WITH THE RECEIVER'S MAC Address
+//uint8_t broadcastAddress[] = {0x48, 0xE7, 0x29, 0xB6, 0x94, 0x50};
+uint8_t broadcastAddress[] = {0xC8,0xC9,0xA3,0xFD,0x02,0x40};
+
+//setup of the message sent to the reciever
+typedef struct struct_message {
+  int id = 1;  // must be unique for each sender board
+  int E_STOP;
+  int GRIPPER;
+  int CAT;
+  int LIFT;
+  int e = 255;
+  int f;
+  int g = 2;
+} struct_message;
+
+struct_message myData;
 
 //Screen set-up
 #define SCREEN_WIDTH 128
@@ -31,17 +49,34 @@ Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 //Variables
 unsigned long screenTimer= 0;
-bool servoOpen = true;
 float batteryPercent;
 bool batteryLow;
 float currentLevel;
-int lierGear;
-int katGear;
 int maxVDeadzone;
 int minVDeadzone;
 int maxHDeadzone;
 int minHDeadzone;
+int lierGear;
+int katGear;
+int servoHoek;
 
+// Create peer interface
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  //Serial.print("\r\nLast Packet Send Status:\t");
+ // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  setWifiLED(status);
+}
+
+void setWifiLED(bool status) {
+  if (status == ESP_NOW_SEND_SUCCESS) {  //LED_Wifi ON/OFF
+    digitalWrite(ledGreen,1);
+  } else {
+    digitalWrite(ledGreen,0);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -66,8 +101,31 @@ void setup() {
 
   Serial.begin(9600); //start seriele monitor
 
+  WiFi.mode(WIFI_STA);
+  printMACadress();
   
-    // initialize OLED display with address 0x3C for 128x64
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  // initialize OLED display with address 0x3C for 128x64
   if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed")); // print een error uit als het upzette van het schermpje niet lukt
     while (true);
@@ -86,7 +144,8 @@ void setup() {
 void updateLierGear(){
     int temp = analogRead(vertLeft); 
         if(temp< minVDeadzone){
-      lierGear=map(temp, 0, minVDeadzone, -650, -1);
+      lierGear =  map(temp, 0, minVDeadzone, -650, -1);
+
     }
     else if (temp > maxVDeadzone){
       lierGear= map(temp, maxVDeadzone, 4095, 1, 650);
@@ -102,7 +161,8 @@ void updateLierGear(){
 void updateKatGear(){
     int temp = analogRead(horRight); 
     if(temp< minHDeadzone){
-      katGear=map(temp, 0, minHDeadzone, -650, -1);
+      katGear =  map(temp, 0, minVDeadzone, -650, -1);
+      
     }
     else if (temp > maxHDeadzone){
       katGear= map(temp, maxHDeadzone, 4095, 1, 650);
@@ -147,11 +207,11 @@ void screenUpdate(){
   oled.print(batteryPercent);// text to display  
   oled.println("%");// text to display
 
-  if (servoOpen == true){
+  if (servoHoek =1000){
     oled.setCursor(100, 0);        // position to display
     oled.println("Open");          // text to display
   }
-  else{
+  else if (servoHoek =2000) {
     oled.setCursor(90,0);         // position to display
     oled.println("closed");       // text to display
   }
@@ -159,6 +219,54 @@ void screenUpdate(){
   screenTimer= millis();          // pakt de huidige tijd
 }
 
+void updateSendValues() {
+  myData.GRIPPER = servoHoek;
+  myData.CAT = katGear;
+  myData.LIFT = lierGear;
+  myData.e += 1;
+  myData.f -= 3;
+  myData.g *= 2;
+  if (myData.e > 9999) myData.e = 255;  // overflow protection
+  if (myData.f < -999)  myData.f = 2;    // overflow protection
+  if (myData.g > 99999) myData.g = 2;    // overflow protection
+
+}
+
+void  SendData(){
+  myData.id = 1;
+  Serial.println("---------------");
+  Serial.printf("id             value: %d \n", myData.id);
+  Serial.printf("E_stop         value: %d \n", myData.E_STOP);
+  Serial.printf("Servo_GRIPPER  value: %d \n", myData.GRIPPER);
+  Serial.printf("Stepper_CAT    value: %d \n", myData.CAT);  //nema 17
+  Serial.printf("Stepper_LIFT   value: %d \n", myData.LIFT);
+  Serial.printf("JoystickL      value: %d \n", myData.e);  //nema 23
+  Serial.printf("JoystickR      value: %d \n", myData.f);
+  Serial.printf("Spare          value: %d \n", myData.g);
+  Serial.println("---------------");
+
+}
+
+void printMACadress() {
+  Serial.printf("Sending to address: { 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X };",
+                broadcastAddress[0],
+                broadcastAddress[1],
+                broadcastAddress[2],
+                broadcastAddress[3],
+                broadcastAddress[4],
+                broadcastAddress[5]);
+  Serial.println("\n");
+}
+
+void servoUpdate(){
+  if(buttonOpen == HIGH){
+    servoHoek = 1000;
+  }
+  else if (buttonClose == HIGH) {
+  servoHoek= 2000;
+  }
+
+}
 void loop() {
   // put your main code here, to run repeatedly:
 
@@ -171,15 +279,23 @@ void loop() {
   if(batteryLow == true){                 // zet de rooie led aan als de battarij bijna leeg is
     digitalWrite(redLed, HIGH);
   }
-  if(buttonClose == HIGH){                // sluit de grijper als de knop ingedrukt is
-    servoOpen= false;
-  }
-  if (buttonOpen == HIGH){                // opent de servo als de knop ingedrukt wordt
-    servoOpen = true;
+  //Sero hoek bepalen
+  if (buttonOpen == HIGH || buttonClose == HIGH){
+      servoUpdate();
   }
   if(buttonStop == HIGH){                 // stopt de loop als de noodstop ingedrukt wordt
     exit(0);
   }
   updateLierGear();                     // roept de functie aan om de versnelling van de lier te hernieuwen
   updateKatGear();                      // roept de functie aan om de versnelling van de kat te hernieuwen
+  updateSendValues();
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  } else {
+    Serial.println("Error sending the data");
+  }
+  SendData();
 }
